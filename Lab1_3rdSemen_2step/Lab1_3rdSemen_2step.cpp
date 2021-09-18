0c2f98338d3d7bc397e06ad676f4998ef0d344a0
rahmatullintimur7777@gmail.com <rahmatullintimur7777@gmail.com>
2021-09-18 15:59:54 +0300

    change 2step

--- Lab1_3rdSemen_2step/Lab1_3rdSemen_2step.cpp
+++ Lab1_3rdSemen_2step/Lab1_3rdSemen_2step.cpp
@@ -12,7 +12,7 @@ int calc(point&, point&, point&);
 
 int main()
 {
-    if (is_convex(5, point{ 2, 4 }, point{ -1, 8 }, point{ 0, 19 }, point{ 4, 14 }, point{ 3, 9 }))
+    if (is_convex(4, point{ 0, 2 }, point{ 0, -5 }, point{ -2, 2 }, point{ -1, -1 }))
         cout << "polygon is convex";
     else
         cout << "polygon is not convex";
@@ -21,56 +21,62 @@ int main()
 bool is_convex(int count, ...)
 {
     va_list argptr;
-    va_start(argptr, count);
-    point first, second, last, prelast, p1, p2, p3;
-
-    first = va_arg(argptr, point);
-    second = va_arg(argptr, point);
-
-    p1 = first;
-    p2 = second;
-
-    bool neg = false;
-    bool pos = false;
-
-    int crossProd = 0;
-
-    for (; count - 2; count--)
+    __try
     {
-        p3 = va_arg(argptr, point);
-        if (count - 2 == 2)
-            prelast = p3;
-        if (count - 2 == 1)
-            last = p3;
-        crossProd = calc(p1, p2, p3);
-        if (crossProd < 0) 
+        va_start(argptr, count);
+        point first, second, last, prelast, p1, p2, p3;
+
+        first = va_arg(argptr, point);
+        second = va_arg(argptr, point);
+
+        p1 = first;
+        p2 = second;
+
+        bool neg = false;
+        bool pos = false;
+
+        int crossProd = 0;
+
+        for (; count - 2; count--)
+        {
+            p3 = va_arg(argptr, point);
+            if (count - 2 == 2)
+                prelast = p3;
+            if (count - 2 == 1)
+                last = p3;
+            crossProd = calc(p1, p2, p3);
+            if (crossProd < 0)
+                neg = true;
+            else if (crossProd > 0)
+                pos = true;
+
+            if (neg && pos) return false; // произошел поворот в разные стороны
+            p1 = p2;
+            p2 = p3;
+        }
+
+        crossProd = calc(prelast, last, first);
+        if (crossProd < 0)
             neg = true;
         else if (crossProd > 0)
             pos = true;
 
-        if (neg && pos) return false; // произошел поворот в разные стороны
-        p1 = p2;
-        p2 = p3;
+        if (neg && pos) return false;
+
+        crossProd = calc(last, first, second);
+        if (crossProd < 0)
+            neg = true;
+        else if (crossProd > 0)
+            pos = true;
+
+        if (neg && pos) return false;
+
+        return true;
     }
-
-    crossProd = calc(prelast, last, first);
-    if (crossProd < 0)
-        neg = true;
-    else if (crossProd > 0)
-        pos = true;
-
-    if (neg && pos) return false;
-
-    crossProd = calc(last, first, second);
-    if (crossProd < 0)
-        neg = true;
-    else if (crossProd > 0)
-        pos = true;
-
-    if (neg && pos) return false;
-
-    va_end(argptr);
-    return true;
+    __finally
+    {
+        va_end(argptr);
+    }
 }
 
 
